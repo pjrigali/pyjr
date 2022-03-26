ef799f54a60453ed14cd7cbca1d1f52f92e987db
Peter <prigali>
2022-03-26 02:47:28 -0500

    Updated requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_file(*parts):
 
 setup(
     name='pyjr',
-    version='0.0.1',
+    version='0.0.2',
     packages=['pyjr', ],
     author='Peter Rigali',
     author_email='peterjrigali@gmail.com',
